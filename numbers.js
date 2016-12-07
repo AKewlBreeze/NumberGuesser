1e0fa800a4b8ef46b6afeb4131f454ca53fb0535
Breana Knight <Akewlbreeze.bc@gmail.com>
2016-12-07 16:53:47 -0700

    adding number population in the populate div. Adjusts the css styling in the population div to render and suggested by the prompt

---
+++ numbers.js
@@ -0,0 +1,11 @@
+
+
+var recentGuess = document.querySelector(".userInput");
+var guessButton = document.querySelector(".guess");
+var clearButton = document.querySelector(".clear");
+var display = document.querySelector(".populate")
+
+guessButton.addEventListener("click",function(){
+  console.log(recentGuess.value);
+  display.innerText=recentGuess.value;
+});
