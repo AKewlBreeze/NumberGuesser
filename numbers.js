1e1c4ab2c1aad3a056d08ebd0e257979f25b58dc
Breana Knight <Akewlbreeze.bc@gmail.com>
2016-12-08 13:20:47 -0700

    adding disable features and minor css changes

--- numbers.js
+++ numbers.js
@@ -3,27 +3,63 @@ var recentGuess = document.querySelector(".userInput");
 var guessButton = document.querySelector(".guess");
 var clearButton = document.querySelector(".clear");
 var display = document.querySelector(".populate");
+var rando = Math.floor (Math.random() * 100);
+var resetButton = document.querySelector(".resetGuess");
+var message = document.querySelector(".message");
 
 
+
+function getRando(){
+  var numberGuess = recentGuess.value;
+  if (numberGuess > 100){
+    alert("Number 1-100 please");
+    display.innerText=("");
+  } else if (numberGuess > rando){
+    alert("too high");
+    display.innerText=(recentGuess.value);
+  } else if (numberGuess < rando){
+    alert("too low");
+    display.innerText=(recentGuess.value);
+  } else if (numberGuess == rando){
+    alert("boom!");
+    display.innerText=(recentGuess.value);
+  }
+}
+
+// if(inputName.val() === ""){
+//   submitButton.prop("disabled", true);
+// } else {
+//     submitButton.prop("disabled", false);
+//   }
+// });
 
 guessButton.addEventListener("click",function(){
-  display.innerText=(recentGuess.value);
-  var numberGuess = recentGuess.value;
-  getRando();
+  if (recentGuess.value === ""){
+    guessButton.disabled=true;
+  } else {
+    guessButton.disabled=false;
+    getRando();
+  }
 
-  function getRando(){
-    var rando = Math.floor (Math.random() * 100);
-    if (rando < numberGuess){
-      alert("too high");
-    } else if (rando > numberGuess){
-      alert("too low");
-    } else {
-      alert("boom!");
-      }
-      console.log(rando);
-    }
-    if (numberGuess > 100){
-      alert("pick a number between 1-100");
-    }
   }
 );
+
+clearButton.addEventListener("click", function (){
+  recentGuess.value = ("");
+  if (recentGuess.value === ""){
+    clearButton.disabled=true;
+  } else {
+    clearButton.disabled=false;
+  }
+});
+
+resetButton.addEventListener("click", function(){
+  if(display.innerText=== ""){
+    resetButton.disabled=true;
+  }
+  recentGuess.value = ("");
+  display.innerText=("");
+  window.location.reload();
+});
+
+// onClick="window.location.reload()"
